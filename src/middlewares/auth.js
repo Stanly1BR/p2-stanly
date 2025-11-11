7922fdd2bcbfe7a344cd352b4b8dfd864eb433d3
stanly <stanlyalmeidabr@gmail.com>
2025-11-11 12:54:08 -0300

    update 03

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,10 +2,16 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-const secret = process.env.JWT_SECRET;
-
 function authenticate(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
+
+  if (!process.env.JWT_SECRET) {
+    console.error("JWT_SECRET não está definido nas variáveis de ambiente!");
+    return res
+      .status(500)
+      .json({ message: "Erro de configuração do servidor" });
+  }
+
   if (!token) return res.status(401).json({ message: "Token não fornecido" });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
